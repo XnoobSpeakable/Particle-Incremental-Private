73e1c931dd93463ad520b25795ae4e3ed08208d4
jakub791 <jakubkarolmazu@gmail.com>
2023-07-28 15:43:56 +0200

    simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Particle-Incremental/",
-  build: {
-    target: "esnext",
-    sourcemap: true
-  },
-  esbuild: {
-    target: "esnext",
-    mangleProps: /pow10|pow_base|tetrate|pentate|layerAdd|layerAdd10/,
-    mangleQuoted: true
-  },
-  server: {
-    host: "0.0.0.0"
-  }
+    base: "./",
+    build: {
+        target: "esnext",
+        sourcemap: true
+    },
+    esbuild: {
+        target: "esnext",
+        mangleProps: /pow10|pow_base|tetrate|pentate|layerAdd|layerAdd10/,
+        mangleQuoted: true
+    }
 });
