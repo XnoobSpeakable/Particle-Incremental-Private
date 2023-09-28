b56ca6393af32393f137bf44a4ecaf4654d67ec9
jakubk791 <jakubkarolmazur@gmail.com>
2023-09-28 16:03:57 +0200

    fix regex

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     esbuild: {
         target: "esnext",
         mangleProps:
-            /|layerAdd|layerAdd10|fromDecimal|fromNumber|fromString|slog_critical|tetrate_critical|critical_section|fromValue|fromValue_noAlloc|fromComponents|fromComponents_noNormalize|normalize/,
+            /layerAdd|layerAdd10|fromDecimal|fromNumber|fromString|slog_critical|tetrate_critical|critical_section|fromValue|fromValue_noAlloc|fromComponents|fromComponents_noNormalize|normalize/,
         mangleQuoted: true
     }
 });
