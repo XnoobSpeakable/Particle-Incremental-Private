4415b0475be43ac63116ae5d16ba3c0c596decc7
XnoobSpeakable <xnoobspeakable@gmail.com>
2023-09-28 17:04:51 +0300

    mangleProps fix?

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
     base: "./",
     build: {
-        target: "esnext"
+        target: "esnext",
+        sourcemap: true
     },
     esbuild: {
         target: "esnext",
         mangleProps:
-            /|layerAdd|layerAdd10|fromDecimal|fromNumber|fromString|slog_critical|tetrate_critical|critical_section|fromValue|fromValue_noAlloc|fromComponents|fromComponents_noNormalize|normalize/,
+        /layerAdd|layerAdd10|fromDecimal|fromNumber|fromString|slog_critical|tetrate_critical|critical_section|fromValue|fromValue_noAlloc|fromComponents|fromComponents_noNormalize|normalize/,
         mangleQuoted: true
     }
 });
