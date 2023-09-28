6e1403e2ff7adbe93c533dc06e8ac63a625366b4
jakubk791 <jakubkarolmazur@gmail.com>
2023-09-28 14:00:06 +0200

    Change property mangling to no break our code

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     esbuild: {
         target: "esnext",
         mangleProps:
-            /pow10|pow_base|tetrate|pentate|layerAdd|layerAdd10|fromDecimal|fromNumber|fromString|slog|slog_critical|tetrate_critical|critical_section|fromValue|fromValue_noAlloc|fromComponents|fromComponents_noNormalize|normalize/,
+            /|layerAdd|layerAdd10|fromDecimal|fromNumber|fromString|slog_critical|tetrate_critical|critical_section|fromValue|fromValue_noAlloc|fromComponents|fromComponents_noNormalize|normalize/,
         mangleQuoted: true
     }
 });
